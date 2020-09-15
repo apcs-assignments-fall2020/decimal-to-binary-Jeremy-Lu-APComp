5be79edbcf9eae64344700976ccd021bc90007e6
Jeremy-Lu-APComp <21jlu@athenian.org>
2020-09-15 02:37:23 +0000

    Added code for binary to decimal converter

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,14 +1,31 @@
+//Binary to Decimal-- Jeremy Lu -- AP Computer Science
 import java.util.Scanner;
+class MyMain {
+ 
+public static int binaryToDecimal(int binary){
+ 
+    int fin_val = 0; // initiallizes val
+    int a = 0; // initializes power
+    while (true) {
+        if (binary == 0){
 
-public class MyMain {
-    
-    public static int binaryToDecimal(String binary) { 
-        return -1;
-        // REPLACE WITH YOUR CODE HERE
+            break; // If program detects "0", the code breaks (continues to iterate through other variables)
+      } 
+        else {
+          int val = binary%10; //extracts the last digit of inputted value
+          fin_val += val*Math.pow(2, a); //completes the formula for calculating dec digits at certain binary values
+          binary = binary/10; //reduces inputted value per power (ex: 111 becomes 11)
+          a+=1; //adds 1 to each power
+       }
     }
-    
-    
-    public static void main(String[] args) {
-        // YOUR CODE HERE
-    }
+    return fin_val; //returns the final decimal
+  }
+ 
+public static void main(String args[]){
+    Scanner scan = new Scanner(System.in);
+    String str = scan.next();
+    System.out.println("Please input a binary value: "); //prompts user to input binary value
+    int user_input = scan.nextInt();
+    System.out.println("Decimal representation of inputted binary value: " + binaryToDecimal(user_input));
+  } //outputs user friendly message
 }
